e79c189e549413550edf8bc2df84cfecdb9c571f
shg <wangshijiehh@outlook.com>
2024-04-05 23:54:43 +0800

    style: 主页导航栏样式

--- src/components/IndexNavbar.vue
+++ src/components/IndexNavbar.vue
@@ -1,15 +1,15 @@
 <template>
   <view class="index-navbar">
-    <view class="fixed flex justify-between w-100vw pt-2 h-8 bg-white">
-      <view class="top-title pl-4 text-lg" :class="{ 'slide-up': isSlideUp }">{{ title }}</view>
+    <view class="z-50 fixed flex justify-between w-100vw pt-2 h-8 bg-white">
+      <view class="top-title pl-30rpx text-lg" :class="{ 'slide-up': isSlideUp }">{{ title }}</view>
       <view></view>
       <view class="more pr-4">
         <wd-icon name="more" color="#f9233b"></wd-icon>
       </view>
     </view>
-    <view class="main pt-10 mx-4">
-      <view class="title text-2xl font-medium">
-        主页
+    <view class="pt-90rpx mx-30rpx">
+      <view class="text-3xl font-700">
+        {{ title }}
         <hr />
       </view>
     </view>
@@ -21,6 +21,7 @@ defineProps<{
   title: string
 }>()
 const isSlideUp = ref(false)
+// TODO 根据滚动距离控制是否显示标题
 onPageScroll(({ scrollTop }) => {
   console.log(scrollTop)
 })
