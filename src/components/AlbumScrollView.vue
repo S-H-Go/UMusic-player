07335f3a89096510ee4547521c29a0cddd5837dd
shg <wangshijiehh@outlook.com>
2024-04-05 23:55:16 +0800

    feat: 添加专辑轮播组件

---
+++ src/components/AlbumScrollView.vue
@@ -0,0 +1,47 @@
+<template>
+  <view class="">
+    <view class="title">
+      <view class="font-600 text-38rpx ml-30rpx mt-40rpx mb-35rpx">{{ title }}</view>
+    </view>
+    <swiper scroll-x class="h-430rpx" :display-multiple-items="2">
+      <swiper-item v-for="item in data" class="w-356rpx" :key="item[keyField]">
+        <MiniAlbum
+          :src="item[srcField]"
+          :name="item[nameField]"
+          :author="item[authorField]"
+        ></MiniAlbum>
+      </swiper-item>
+    </swiper>
+  </view>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  title: String,
+  isNav: Boolean,
+  navUrl: {
+    type: String,
+    default: '',
+  },
+  data: Array,
+  keyField: String,
+  srcField: String,
+  nameField: String,
+  authorField: String,
+})
+</script>
+
+<style lang="scss" scoped>
+.title-text {
+  font-size: 40rpx;
+  font-weight: bold;
+}
+
+:deep(.uni-swiper-slides) {
+  inset: 0 30rpx;
+}
+
+:deep(.uni-swiper-slide-frame) {
+  width: 352rpx !important;
+}
+</style>
---
+++ src/components/MiniAlbum.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="mini-albulm" @click="to">
+    <wd-img :src="src" class="w-336rpx h-336rpx" :radius="10"></wd-img>
+    <div class="text-26rpx truncate mx-4rpx">{{ name }}</div>
+    <view class="text-26rpx text-[#929296] truncate mx-4rpx">{{ author }}</view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  toUrl: {
+    type: String,
+    default: '',
+  },
+  src: String,
+  name: String,
+  author: String,
+})
+function to() {
+  uni.navigateTo({
+    url: props.toUrl,
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
