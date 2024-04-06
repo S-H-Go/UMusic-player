4470f22e4aef5cc56a639ebb200bebe82d1239dc
shg <wangshijiehh@outlook.com>
2024-04-06 23:44:34 +0800

    feat: 添加导航栏组件

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <view class="w-100vw h-105rpx flex items-center justify-between">
+    <view class="w-105rpx center" @click="back">
+      <wd-icon
+        name="arrow-left1"
+        custom-style="font-weight: 700;"
+        color="#fa233b"
+        size="32rpx"
+      ></wd-icon>
+    </view>
+    <view class="title flex-1 ml-32rpx">
+      <view class="text-34rpx truncate max-w-450rpx">{{ title }}</view>
+    </view>
+    <view class="more center w-90rpx h-90rpx">
+      <wd-icon name="more" custom-style="font-weight: 700;" color="#fa233b" size="32rpx"></wd-icon>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  title: {
+    type: String,
+  },
+})
+
+function back() {
+  uni.navigateBack()
+}
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
